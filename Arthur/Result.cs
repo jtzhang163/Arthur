83456b3230606a652204d707a5c948f20a1d9c3d
张建通 <jtzhang163@163.com>
2019-03-08 21:49:57 +0800

    文件基本架构完成

---
+++ Arthur/Result.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arthur
+{
+
+    public class Result
+    {
+
+        public int Code { get; set; }
+
+        public string Msg { get; set; }
+
+        public bool IsOk
+        {
+            get
+            {
+                return this.Code == 0;
+            }
+        }
+
+        public Result()
+        {
+
+        }
+
+        public Result(string msg) : this(-1, msg)
+        {
+
+        }
+
+        public Result(int code, string msg)
+        {
+            this.Code = code;
+            this.Msg = msg;
+        }
+
+        public static Result OK
+        {
+            get
+            {
+                return new Result(0, string.Empty);
+            }
+        }
+
+    }
+}
