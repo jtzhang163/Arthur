fc959f3df9ddbab5fd167740ecfd98479fd41d7e
张建通 <jtzhang163@163.com>
2019-03-09 16:24:49 +0800

    登录界面和页面顶部栏

---
+++ Arthur.View/Account/LoginUC.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arthur.View.Account
+{
+    /// <summary>
+    /// LoginUC.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginUC : UserControl
+    {
+        public LoginUC()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            //lbTip.Content = "用户名或密码错误！";
+            //lbTip.Visibility = Visibility.Visible;
+
+            Window parentWindow = Window.GetWindow(this);
+            Console.WriteLine(parentWindow.Title);
+
+            Type type = parentWindow.GetType();
+            MethodInfo mi = type.GetMethod("LoginSuccessInvoke");
+
+            mi.Invoke(parentWindow, new object[] { });
+        }
+
+        private void Input_GotFocus(object sender, RoutedEventArgs e)
+        {
+            lbTip.Visibility = Visibility.Hidden;
+        }
+    }
+}
--- Clients/SzYitong/SzYitong.Bis.App/AccountWindow.xaml.cs
+++ Clients/SzYitong/SzYitong.Bis.App/AccountWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Arthur.View.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,42 @@ namespace SzYitong.Bis.App
     /// <summary>
     /// AccountWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class AccountWindow : Window
+    public partial class AccountWindow : Window, IAccountView
     {
-        public AccountWindow()
+        public string Option { get; set; }
+
+        public AccountWindow() : this("login")
         {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="option">登录：login; 注册：register</param>
+        public AccountWindow(string option)
+        {
+            this.Option = option;
             InitializeComponent();
+
+            //----------------------动态添加用户控件 登录/注册-------------------------
+            if(this.Option == "login")
+            {
+                LoginUC loginUC = new LoginUC();
+                loginUC.Height = 300;
+                loginUC.Width = 450;
+                loginUC.HorizontalAlignment = HorizontalAlignment.Center;
+                loginUC.VerticalAlignment = VerticalAlignment.Center;
+                grid.Children.Add(loginUC);
+            }
+
+        }
+
+
+        public void LoginSuccessInvoke()
+        {
+            new MainWindow().Show();
+            this.Close();
         }
     }
 }
