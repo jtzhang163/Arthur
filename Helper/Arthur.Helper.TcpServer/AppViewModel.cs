af8d212c5f554fb235618e103ffb0a2b2a787c45
张建通 <jtzhang163@163.com>
2019-03-24 15:52:12 +0800

    TCP Server 调试助手

---
+++ Helper/Arthur.Helper.TcpServer/AppViewModel.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arthur.Helper.TcpServer
+{
+    public class AppViewModel : BindableObject
+    {
+        private bool isRunning = false;
+
+        public bool IsRunning
+        {
+            get => isRunning;
+            set => SetProperty(ref isRunning, value);
+        }
+
+        private string tip = string.Empty;
+
+        public string Tip
+        {
+            get => tip;
+            set => SetProperty(ref tip, value);
+        }
+
+
+        private string ip = string.Empty;
+
+        public string IP
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ip))
+                {
+                    ip = ConfigurationManager.AppSettings["IP"];
+                }
+                return ip;
+            }
+            set
+            {
+                if (!ip.Equals(value))
+                {
+                    AppHelper.SetConfig("IP", value);
+                    SetProperty(ref ip, value);
+                }
+            }
+        }
+
+        private int port = -1;
+
+        public int Port
+        {
+            get
+            {
+                if (port < 0)
+                {
+                    port = Convert.ToInt32(ConfigurationManager.AppSettings["Port"]);
+                }
+                return port;
+            }
+            set
+            {
+                if (port != value)
+                {
+                    AppHelper.SetConfig("Port", value.ToString());
+                    SetProperty(ref port, value);
+                }
+            }
+        }
+
+        private string receive1 = string.Empty;
+
+        public string Receive1
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(receive1))
+                {
+                    receive1 = ConfigurationManager.AppSettings["Receive1"];
+                }
+                return receive1;
+            }
+            set
+            {
+                if (!receive1.Equals(value))
+                {
+                    AppHelper.SetConfig("Receive1", value);
+                    SetProperty(ref receive1, value);
+                }
+            }
+        }
+
+        private string send1 = string.Empty;
+
+        public string Send1
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(send1))
+                {
+                    send1 = ConfigurationManager.AppSettings["Send1"];
+                }
+                return send1;
+            }
+            set
+            {
+                if (!send1.Equals(value))
+                {
+                    AppHelper.SetConfig("Send1", value);
+                    SetProperty(ref send1, value);
+                }
+            }
+        }
+
+
+        private string receive2 = string.Empty;
+
+        public string Receive2
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(receive2))
+                {
+                    receive2 = ConfigurationManager.AppSettings["Receive2"];
+                }
+                return receive2;
+            }
+            set
+            {
+                if (!receive2.Equals(value))
+                {
+                    AppHelper.SetConfig("Receive2", value);
+                    SetProperty(ref receive2, value);
+                }
+            }
+        }
+
+        private string send2 = string.Empty;
+
+        public string Send2
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(send2))
+                {
+                    send2 = ConfigurationManager.AppSettings["Send2"];
+                }
+                return send2;
+            }
+            set
+            {
+                if (!send2.Equals(value))
+                {
+                    AppHelper.SetConfig("Send2", value);
+                    SetProperty(ref send2, value);
+                }
+            }
+        }
+
+
+        private string receive3 = string.Empty;
+
+        public string Receive3
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(receive3))
+                {
+                    receive3 = ConfigurationManager.AppSettings["Receive3"];
+                }
+                return receive3;
+            }
+            set
+            {
+                if (!receive3.Equals(value))
+                {
+                    AppHelper.SetConfig("Receive3", value);
+                    SetProperty(ref receive3, value);
+                }
+            }
+        }
+
+        private string send3 = string.Empty;
+
+        public string Send3
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(send3))
+                {
+                    send3 = ConfigurationManager.AppSettings["Send3"];
+                }
+                return send3;
+            }
+            set
+            {
+                if (!send3.Equals(value))
+                {
+                    AppHelper.SetConfig("Send3", value);
+                    SetProperty(ref send3, value);
+                }
+            }
+        }
+
+
+        private string receive4 = string.Empty;
+
+        public string Receive4
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(receive4))
+                {
+                    receive4 = ConfigurationManager.AppSettings["Receive4"];
+                }
+                return receive4;
+            }
+            set
+            {
+                if (!receive4.Equals(value))
+                {
+                    AppHelper.SetConfig("Receive4", value);
+                    SetProperty(ref receive4, value);
+                }
+            }
+        }
+
+        private string send4 = string.Empty;
+
+        public string Send4
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(send4))
+                {
+                    send4 = ConfigurationManager.AppSettings["Send4"];
+                }
+                return send4;
+            }
+            set
+            {
+                if (!send4.Equals(value))
+                {
+                    AppHelper.SetConfig("Send4", value);
+                    SetProperty(ref send4, value);
+                }
+            }
+        }
+
+    }
+}
---
+++ Helper/Arthur.Helper.TcpServer/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arthur.Helper.TcpServer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = Current.App;
+        }
+
+        TcpListener server = null;
+        byte[] bytes = new byte[256];
+        string data = null;
+
+        public void Run()
+        {
+
+            try
+            {
+                while (Current.App.IsRunning)
+                {
+                    ReceiveSendDataAdd("Waiting for a connection... ");
+                    TcpClient client = server.AcceptTcpClient();
+                    ReceiveSendDataAdd("Connected!");
+                    data = null;
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        ReceiveSendDataAdd("Received: " + data.Trim(new char[] { '\r', '\n' }));
+                        if (data.Contains(Current.App.Receive1))
+                        {
+                            byte[] send = System.Text.Encoding.ASCII.GetBytes(Current.App.Send1 + "\r");
+                            stream.Write(send, 0, send.Length);
+                            ReceiveSendDataAdd("Sent: " + Current.App.Send1);
+                        }
+                        else if (data.Contains(Current.App.Receive2))
+                        {
+                            byte[] send = System.Text.Encoding.ASCII.GetBytes(Current.App.Send2 + "\r");
+                            stream.Write(send, 0, send.Length);
+                            ReceiveSendDataAdd("Sent: " + Current.App.Send2);
+                        }
+                        else if (data.Contains(Current.App.Receive3))
+                        {
+                            byte[] send = System.Text.Encoding.ASCII.GetBytes(Current.App.Send3 + "\r");
+                            stream.Write(send, 0, send.Length);
+                            ReceiveSendDataAdd("Sent: " + Current.App.Send3);
+                        }
+                        else if (data.Contains(Current.App.Receive4))
+                        {
+                            byte[] send = System.Text.Encoding.ASCII.GetBytes(Current.App.Send4 + "\r");
+                            stream.Write(send, 0, send.Length);
+                            ReceiveSendDataAdd("Sent: " + Current.App.Send4);
+                        }
+
+                    }
+
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                ReceiveSendDataAdd("SocketException:" + e);
+                ReceiveSendDataAdd("Hit enter to continue...");
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.Stop();
+                }
+            }
+
+        }
+
+        public void ReceiveSendDataAdd(string info)
+        {
+            Current.App.Tip += info + "\r\n";
+        }
+
+
+        private void btnListen_Click(object sender, RoutedEventArgs e)
+        {
+
+            Current.App.IsRunning = !Current.App.IsRunning;
+
+            if (Current.App.IsRunning)
+            {
+                server = new TcpListener(IPAddress.Parse(Current.App.IP), Current.App.Port);
+                server.Start();
+
+                var t = new Thread(() => {
+                    Run();
+                });
+                t.Start();
+            }
+            else
+            {
+
+                server.Stop();
+                server = null;
+            }
+        }
+
+        private void btnClearShow_Click(object sender, RoutedEventArgs e)
+        {
+            Current.App.Tip = "";
+        }
+    }
+}
