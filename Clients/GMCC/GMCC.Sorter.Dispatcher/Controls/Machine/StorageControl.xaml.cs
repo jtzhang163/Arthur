1241ae6e95aadb4df6004353f637d9cc57e6ad54
张建通 <jtzhang163@163.com>
2019-04-11 17:56:29 +0800

    右键手动任务

--- Clients/GMCC/GMCC.Sorter.Dispatcher/Controls/Machine/StorageControl.xaml.cs
+++ Clients/GMCC/GMCC.Sorter.Dispatcher/Controls/Machine/StorageControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using GMCC.Sorter.Dispatcher.Utils;
+﻿using GMCC.Sorter.Data;
+using GMCC.Sorter.Dispatcher.Utils;
 using GMCC.Sorter.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -22,7 +23,7 @@ namespace GMCC.Sorter.Dispatcher.Controls.Machine
     /// </summary>
     public partial class StorageControl : UserControl
     {
-        private StorageViewModel storage;
+        private StorageViewModel Storage;
 
         public int Col;
         public int Floor;
@@ -31,15 +32,50 @@ namespace GMCC.Sorter.Dispatcher.Controls.Machine
         {
 
             InitializeComponent();
-            storage = Current.Storages.FirstOrDefault(o => o.Id == id);
-            this.DataContext = storage;
-            this.Col = storage.Column;
-            this.Floor = storage.Floor;
+            this.Storage = Current.Storages.FirstOrDefault(o => o.Id == id);
+            this.DataContext = this.Storage;
+            this.Col = this.Storage.Column;
+            this.Floor = this.Storage.Floor;
         }
 
         private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            ShowWindows.ShowTrayBatteryWin(storage.ProcTrayId);
+            ShowWindows.ShowTrayBatteryWin(this.Storage.ProcTrayId);
+        }
+
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //右击时
+            if(e.ChangedButton == MouseButton.Right)
+            {
+                if (Current.App.TaskMode == TaskMode.自动任务)
+                    return;
+
+                ContextMenu cm = this.FindResource("manu_get_put") as ContextMenu;
+                cm.PlacementTarget = sender as Button;
+                cm.IsOpen = true;
+
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+            var header = (sender as MenuItem).Header.ToString();
+            if (header == "手动【上料】" || header == "手动【下料】")
+            {
+                if (Current.Task.Status != Model.TaskStatus.完成)
+                {
+                    MessageBox.Show("当前任务尚未完成！","提示");
+                }
+                Current.Task.StorageId = this.Storage.Id;
+                Current.Task.Type = header == "手动【上料】" ? Model.TaskType.上料 : Model.TaskType.下料;
+                Current.Task.StartTime = DateTime.Now;
+                Current.Task.ProcTrayId = Current.Task.Type == Model.TaskType.上料 ? Current.MainMachine.ChargeProcTrayId : this.Storage.ProcTrayId;
+                Current.Task.Status = Model.TaskStatus.就绪;
+                Context.AppContext.SaveChanges();
+            }
         }
     }
 }
